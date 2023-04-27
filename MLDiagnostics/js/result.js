3db8854ad34f3b06834becf493d8f54c2b1550e0
mackoyyakooo <115718048+mackoyyakooo@users.noreply.github.com>
2023-04-27 18:57:25 +0800

    Update result.js

--- MLDiagnostics/js/result.js
+++ MLDiagnostics/js/result.js
@@ -3,14 +3,14 @@ function result() {
 
 	var patient= {
 
-		PAMUGASMARKALFRED0703 : {
+		"PAMUGASMARKALFRED0703" : {
 			link: 'Results'
 		},
 
-		DELACRUZJUAN01012001 : {
+		"DELACRUZJUAN01012001" : {
 			link: 'Results/Pamugas, Mark Alfred.pdf'
 		},
-		ML-000001 : {
+		"ML-000001" : {
 			link: 'Results/Pamugas, Mark Alfred.pdf'
 		},
 	}
