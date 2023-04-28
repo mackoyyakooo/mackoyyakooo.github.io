40332b7ee73548fd78ba61c98ad50d9e409bf9cf
mackoyyakooo <akosisinichiie@gmail.com>
2023-04-28 18:15:09 +0800

    Update result.js

--- MLDiagnostics/js/result.js
+++ MLDiagnostics/js/result.js
@@ -1,29 +1,25 @@
 function result() {
-	//Patient
+    // Patient data
+    var patient = {
+        "PAMUGASMARKALFRED0703": {
+            link: 'Results/PAMUGAS, MARK ALFRED/' // <-- Modify this line to include the folder path or URL
+        },
+        "DELACRUZJUAN01012001": {
+            link: 'Results/Pamugas, Mark Alfred.pdf'
+        },
+        "ML-000001": {
+            link: 'Results/Pamugas, Mark Alfred.pdf'
+        },
+    }
 
-	var patient= {
+    var patientname = document.getElementById('patientname').value;
+    var input = patientname.toUpperCase();
+    var definition = patient[input];
+    var output = document.getElementById("output");
 
-		"PAMUGASMARKALFRED0703" : {
-			link: 'Results/PAMUGAS, MARK ALFRED'
-		},
-
-		"DELACRUZJUAN01012001" : {
-			link: 'Results/Pamugas, Mark Alfred.pdf'
-		},
-		"ML-000001" : {
-			link: 'Results/Pamugas, Mark Alfred.pdf'
-		},
-	}
-
-
-	var patientname = document.getElementById('patientname').value;
-	var input = patientname.toUpperCase();
-	var definition = patient[input];
-	var output = document.getElementById("output");
-
-	if (definition==undefined) {
-		output.innerHTML='<hr>There is no information about this patient.<hr>';
-	} else {
-		output.innerHTML='<hr><a href="'+definition.link+'">Click here to view result</a><br><br><br>For the password of the file, kindly input your LASTNAME FIRSTNAME and your BIRTHDAY (mmddyyy) Ex. DELACRUZJUAN01011990. Thank you.<hr>'
-	};
+    if (definition == undefined) {
+        output.innerHTML = '<hr>There is no information about this patient.<hr>';
+    } else {
+        output.innerHTML = '<hr><a href="' + definition.link + '">Click here to view result</a><br><br><br>For the password of the file, kindly input your LASTNAME FIRSTNAME and your BIRTHDAY (mmddyyy) Ex. DELACRUZJUAN01011990. Thank you.<hr>'
+    };
 };
