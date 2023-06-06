2a20df0c465549d7d3514611bd507b9d9c4c69bb
mackoyyakooo <akosisinichiie@gmail.com>
2023-06-06 20:38:45 +0800

    asdas

--- MLDiagnostics/js/result.js
+++ MLDiagnostics/js/result.js
@@ -1,18 +1,11 @@
 function result() {
-    // Patient data
-    var patient = {
-        "ML-000001": {
-            link: 'Results/PAMUGAS, MARK ALFRED.zip'
-        },
-        "A": {
-            link: 'Results/PAMUGAS, MARK ALFRED.zip'
-        },
-        "B": {
-            link: 'Results/PAMUGAS, MARK ALFRED.zip'
-        },
-        "C": {
-            link: 'Results/PAMUGAS, MARK ALFRED.zip'
-        },
+    var patient = {};
+
+    for (var i = 1; i <= 1000; i++) {
+        var id = "ML-" + i.toString().padStart(6, "0");
+        patient[id] = {
+            link: 'Results/' + id + '.pdf'
+        };
     }
 
     var patientname = document.getElementById('patientname').value;
@@ -25,4 +18,4 @@ function result() {
     } else {
         output.innerHTML = '<hr><a href="' + definition.link + '">Click here to view result</a><br><br><br>For the password of the file, kindly input your LASTNAME FIRSTNAME and your BIRTHDAY (mmddyyy) Ex. DELACRUZJUAN01011990. Thank you.<hr>'
     };
-};
+}
