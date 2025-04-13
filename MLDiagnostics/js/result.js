8345a55a8b6dbd7cbedcb8b3c6cdc97df30eb996
mackoyyakooo <akosisinichiie@gmail.com>
2025-04-13 18:32:00 +0800

    a

--- MLDiagnostics/js/result.js
+++ MLDiagnostics/js/result.js
@@ -4,7 +4,7 @@ function result() {
     for (var i = 1; i <= 1000; i++) {
         var id = "ML-" + i.toString().padStart(6, "0");
         patient[id] = {
-            link: 'Results/' + id + '/'
+            link: 'Results/' + id + '.zip' // Assuming you have ZIP files in the Results folder
         };
     }
 
@@ -17,7 +17,7 @@ function result() {
         output.innerHTML = '<hr>There is no information about this patient.<hr>';
     } else {
         output.innerHTML =
-            '<hr><a href="' + definition.link + 'report.pdf" download>Click here to download your result (PDF)</a>' +
+            '<hr><a href="' + definition.link + '" download>Click here to download your result (ZIP)</a>' +
             '<br><br><br>For the password of the file, kindly input your <strong>LASTNAME FIRSTNAME</strong> and your <strong>BIRTHDAY (mmddyyyy)</strong> ' +
             '<br><i>Example: DELACRUZJUAN01011990</i><hr>';
     }
