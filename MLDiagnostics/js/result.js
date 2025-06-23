3fbd155d6dae476dd05edcc42e7764a7eaca70ad
mackoyyakooo <akosisinichiie@gmail.com>
2025-06-24 01:31:47 +0800

    a

--- MLDiagnostics/js/result.js
+++ MLDiagnostics/js/result.js
@@ -1,25 +1,23 @@
 function result() {
     var patient = {};
 
-    // Loop to create patient IDs
     for (var i = 1; i <= 1000; i++) {
         var id = "ML-" + i.toString().padStart(6, "0");
         patient[id] = {
-            link: 'Results/' + id + '/'
+            link: 'Results/' + id + '.pdf'
         };
     }
 
-    // Get input value and format
-    var patientname = document.getElementById('patientname').value.trim(); // .trim() to remove any spaces
-    var input = patientname.toUpperCase(); // Convert to uppercase for consistency
+    var patientname = document.getElementById('patientname').value;
+    var input = patientname.toUpperCase();
     var definition = patient[input];
     var output = document.getElementById("output");
 
     if (definition == undefined) {
-        output.innerHTML = '<hr>There is no information about this patient. Please check your Patient ID.<hr>';
+        output.innerHTML = '<hr>There is no information about this patient.<hr>';
     } else {
         output.innerHTML =
-            '<hr><a href="' + definition.link + 'report.pdf" download>Click here to download your result (PDF)</a>' +
+            '<hr><a href="' + definition.link + '" download>Click here to download your result (ZIP)</a>' +
             '<br><br><br>For the password of the file, kindly input your <strong>LASTNAME FIRSTNAME</strong> and your <strong>BIRTHDAY (mmddyyyy)</strong> ' +
             '<br><i>Example: DELACRUZJUAN01011990</i><hr>';
     }
