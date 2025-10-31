acade61626869a66046f0eff316b16cd57e3fcb8
mackoyyakooo <akosisinichiie@gmail.com>
2025-10-31 14:23:32 +0800

    1

--- MLDiagnostics/js/result.js
+++ MLDiagnostics/js/result.js
@@ -1,24 +1,26 @@
 function result() {
-    var patient = {};
+  var patient = {};
 
-    for (var i = 1; i <= 1000; i++) {
-        var id = "ML-" + i.toString().padStart(6, "0");
-        patient[id] = {
-            link: 'Results/' + id + '.pdf'
-        };
-    }
+  for (var i = 1; i <= 1000; i++) {
+    var id = "ML-" + i.toString().padStart(6, "0");
+    patient[id] = {
+      link: "Results/" + id + ".pdf",
+    };
+  }
 
-    var patientname = document.getElementById('patientname').value;
-    var input = patientname.toUpperCase();
-    var definition = patient[input];
-    var output = document.getElementById("output");
+  var patientname = document.getElementById("patientname").value;
+  var input = patientname.trim().toUpperCase();
+  var definition = patient[input];
+  var output = document.getElementById("output");
 
-    if (definition == undefined) {
-        output.innerHTML = '<hr>There is no information about this patient.<hr>';
-    } else {
-        output.innerHTML =
-            '<hr><a href="' + definition.link + '" download>Click here to download your result (ZIP)</a>' +
-            '<br><br><br>For the password of the file, kindly input your <strong>LASTNAME FIRSTNAME</strong> and your <strong>BIRTHDAY (mmddyyyy)</strong> ' +
-            '<br><i>Example: DELACRUZJUAN01011990</i><hr>';
-    }
+  if (!definition) {
+    output.innerHTML = "<hr>❌ There is no information about this patient.<hr>";
+  } else {
+    output.innerHTML =
+      '<hr><a href="' +
+      definition.link +
+      '" download>Click here to download your result (PDF)</a>' +
+      "<br><br><br>For the password of the file, kindly input your <strong>LASTNAMEFIRSTNAME</strong> and your <strong>BIRTHDAY (mmddyyyy)</strong> " +
+      "<br><i>Example: DELACRUZJUAN01011990</i><hr>";
+  }
 }
